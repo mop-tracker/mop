713b87609d4d950fe756d164b8bb4602402eadbc
Michael Dvorkin <mike@dvorkin.net>
2013-07-10 21:27:04 -0700

    Save/load profile

--- lib/profile.go
+++ lib/profile.go
@@ -1,8 +1,56 @@
 // Copyright (c) 2013 by Michael Dvorkin. All Rights Reserved.
 //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 package mop
+import (
+        "strings"
+        "os/user"
+        "io/ioutil"
+        "encoding/json"
+)
+
+const rcfile = "/.moprc"
+
+type Profile struct {
+        MarketRefreshRate int
+        QuotesRefreshRate int
+        Tickers           []string
+        SortBy            string
+        SortOrder         string
+}
+
+var profile Profile
 
 //-----------------------------------------------------------------------------
 func LoadProfile() string {
-	return "AAPL+ALU+ATVI+C+COH+GOOG+HPQ+IBM+MA+TSLA+V+YHOO+ININ+CRM+SAAS"
+        data, err := ioutil.ReadFile(defaultProfile())
+        if err != nil {
+                // Set default values.
+                profile.MarketRefreshRate = 12
+                profile.QuotesRefreshRate = 5
+                profile.Tickers = []string{ "AAPL", "C", "GOOG", "IBM", "KO", "ORCL", "V" }
+                profile.SortBy = "Ticker"
+                profile.SortOrder = "Desc"
+                profile.Save()
+        } else {
+                json.Unmarshal(data, &profile)
+        }
+	return strings.Join(profile.Tickers, "+")
+}
+
+//-----------------------------------------------------------------------------
+func (profile *Profile) Save() error {
+        if data, err := json.Marshal(profile); err != nil {
+                return err
+        } else {
+                return ioutil.WriteFile(defaultProfile(), data, 0644)
+        }
+}
+
+//-----------------------------------------------------------------------------
+func defaultProfile() string {
+        usr, err := user.Current()
+        if err != nil {
+                panic(err)
+        }
+        return usr.HomeDir + rcfile
 }
