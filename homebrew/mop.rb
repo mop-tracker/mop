2edcc829be31deec58c5f176d8b6dd0358fec1d3
Michael Dvorkin <mike@dvorkin.net>
2013-09-11 22:39:09 -0700

    Homebrew formula for v0.1.0

--- homebrew/mop.rb
+++ homebrew/mop.rb
@@ -1,4 +1,16 @@
 require "formula"
 
 class Mop < Formula
+  homepage "https://github.com/michaeldv/mop"
+  head     "https://github.com/michaeldv/mop.git"
+  url      "https://github.com/michaeldv/mop/archive/v0.1.0.tar.gz"
+  sha1     "b3bd5b529430da22bfa1beeca435a25e72513e27"
+
+  depends_on "go" => :build
+
+  def install
+  end
+
+  def caveats
+  end
 end
