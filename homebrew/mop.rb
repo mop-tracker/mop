3f4f100c850293afd081eab26e1babf6bca1c7c4
Brandon Camilleri <brandon.camilleri.90@gmail.com>
2016-12-22 20:28:23 +0100

    Update mop.rb

--- homebrew/mop.rb
+++ homebrew/mop.rb
@@ -1,15 +1,14 @@
 require "formula"
 
 class Mop < Formula
-  homepage "https://github.com/michaeldv/mop"
-  head     "https://github.com/michaeldv/mop.git"
-  url      "https://github.com/michaeldv/mop/archive/v0.1.0.tar.gz"
+  homepage "https://github.com/brandleesee/mop"
+  head     "https://github.com/brandleesee/mop.git"
   sha1     "b3bd5b529430da22bfa1beeca435a25e72513e27"
 
   depends_on "go" => :build
 
   def install
-    system "go", "get", "github.com/michaeldv/termbox-go"
+    system "go", "get", "github.com/brandleesee/termbox-go"
     system "go build cmd/mop.go"
     bin.install "mop"
   end
