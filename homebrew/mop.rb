92022008ec50385bb692426bbc53693c987cd67b
Michael G. Khmelnitsky <mig281@users.noreply.github.com>
2017-04-05 09:09:02 -0700

    The URLs need to be updated to the new Mop repo, right?

--- homebrew/mop.rb
+++ homebrew/mop.rb
@@ -1,9 +1,9 @@
 require "formula"
 
 class Mop < Formula
-  homepage "https://github.com/michaeldv/mop"
-  head     "https://github.com/michaeldv/mop.git"
-  url      "https://github.com/michaeldv/mop/archive/v0.1.0.tar.gz"
+  homepage "https://github.com/mop-tracker/mop"
+  head     "https://github.com/mop-tracker/mop.git"
+  url      "https://github.com/mop-tracker/mop/archive/v0.1.0.tar.gz"
   sha1     "b3bd5b529430da22bfa1beeca435a25e72513e27"
 
   depends_on "go" => :build
